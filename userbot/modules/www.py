121e60f31f53f9d1be054be940f3a3beaa018241
Ultra-men <88121146+Puutraaa@users.noreply.github.com>
2021-08-16 20:06:12 +0800

    Update www.py

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -96,7 +96,7 @@ async def pingme(pong):
 async def pingme(pong):
     """ For .ping command, ping the userbot from any chat.  """
     start = datetime.now()
-    await pong.edit("`gass!`")
+    await pong.edit("`DUARRR!`")
     end = datetime.now()
     duration = (end - start).microseconds / 9000
     await pong.edit("`Ping!\n%sms`" % (duration))
