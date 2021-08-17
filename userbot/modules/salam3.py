9cc19220ac498bee04dd27d31919c9d292ce85e3
Ultra-men <88121146+Puutraaa@users.noreply.github.com>
2021-08-18 07:59:53 +0800

    Update salam3.py

--- userbot/modules/salam3.py
+++ userbot/modules/salam3.py
@@ -7,37 +7,37 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 # ============================================
 
 
-@register(outgoing=True, pattern='^.mmk(?: |$)(.*)')
+@register(outgoing=True, pattern='.mmk(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**ASTAGAAAA MEMEKNYA ANAK INI!!!!**")
 
 
-@register(outgoing=True, pattern='^.ek(?: |$)(.*)')
+@register(outgoing=True, pattern='.ek(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**EH KONTOOOLL!!!**")
 
 
-@register(outgoing=True, pattern='^.ya(?: |$)(.*)')
+@register(outgoing=True, pattern='.ya(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**YAUDAH IYAAA SAYANG...**")
 
 
-@register(outgoing=True, pattern='^.asn(?: |$)(.*)')
+@register(outgoing=True, pattern='.asn(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**ASTAGFIRULLAH NGENTOOOT!!!**")
 
 
-@register(outgoing=True, pattern='^.suci(?: |$)(.*)')
+@register(outgoing=True, pattern='.suci(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**LU LAMA-LAMA JADI KEK ANAK HARAM, KEKNYA HARUS GUA BAPTIS. SINI LU NGENTOT GUA BAPTIS BIAR SUCI JIWA LO YANG HARAM ITU!!!**")
 
 
-@register(outgoing=True, pattern='^.wibu(?: |$)(.*)')
+@register(outgoing=True, pattern='.wibu(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**LARI CUK ADA WIBU!!!**🏃🏃🏃")
