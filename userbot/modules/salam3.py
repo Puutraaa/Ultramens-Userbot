f71beb6822c9ccd5b32cd720447cadd093f61c57
Ultra-men <88121146+Puutraaa@users.noreply.github.com>
2021-08-19 15:34:19 +0800

    Update salam3.py

--- userbot/modules/salam3.py
+++ userbot/modules/salam3.py
@@ -42,6 +42,78 @@ async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**LARI CUK ADA WIBU!!!**🏃🏃🏃")
 
+@register(outgoing=True, pattern='^.gjt(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("APASI NGENTOT GAJELAS LU TOLOL")
+
+@register(outgoing=True, pattern='^.gjn(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("Gajelas Ngentottt")
+
+
+@register(outgoing=True, pattern='^.yb(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**Yabenarrrrrrr...**")
+
+@register(outgoing=True, pattern='^.gjb(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**GAJELAS BABI....**")
+
+
+@register(outgoing=True, pattern='^.gjk(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**Gajelas Kontolll....**")
+
+
+@register(outgoing=True, pattern='^.gbgn(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**Ga banget, Ngentott!!!**")
+
+
+@register(outgoing=True, pattern='^.gls(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**GA, LO SANGEAN!!!**")
+
+
+@register(outgoing=True, pattern='^.bsl(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**BAU SAWI LO..!!**")
+
+
+@register(outgoing=True, pattern='^.hai(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**Hai, Anak yatim!!**")
+
+@register(outgoing=True, pattern='^.ucp(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**Lu siapa si ngentooootttt!!!!**")
+
+
+@register(outgoing=True, pattern='^.hey(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**HAI BOCAH HINA!**")
+
+@register(outgoing=True, pattern='^.mgk(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**MAKANYA GANTENG KONTOL!**")
+
+
+@register(outgoing=True, pattern='^.loh(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**GC SAMPAH KAYA GINI, BUBARIN AJA PLIS!!🤣**")
 
 CMD_HELP.update({
     "salam3":
